7abeb49b862d377317a6ac08aba736f40bfaea37
flowerinthenight <flowerinthenight@pm.me>
2021-09-19 01:26:34 +0900

    chore: wip structure

--- dstore.go
+++ dstore.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/flowerinthenight/spindle"
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 )
 
@@ -16,26 +17,42 @@ type Storer interface {
 
 // Store is our main distributed, append-only log storage object.
 type Store struct {
-	Id      string        // this instance's unique id
-	Locker  *spindle.Lock // handles our distributed lock
-	NatsCon *nats.Conn    // our NATS connection
-	Storer                // our storage backend
+	id            string     // this instance's unique id
+	natsCon       *nats.Conn // our NATS connection
+	*spindle.Lock            // handles our distributed lock
+	Storer                   // our storage backend
 }
 
 // Start starts the main goroutine for *spindle.Lock. It terminates upon
 // 'ctx' cancellation, optionally sending a message to 'done' when finished.
 func (s *Store) Start(ctx context.Context, done ...chan error) error {
-	if s.Locker == nil {
-		return fmt.Errorf("Locker cannot be nil")
+	if s.natsCon == nil {
+		return fmt.Errorf("NatsCon cannot be nil")
+	}
+
+	if s.Lock == nil {
+		return fmt.Errorf("Lock cannot be nil")
 	}
 
 	if s.Storer == nil {
 		return fmt.Errorf("Storer cannot be nil")
 	}
 
-	if s.NatsCon == nil {
-		return fmt.Errorf("NatsCon cannot be nil")
+	return nil
+}
+
+type Config struct {
+	Id      string // will generate uuid if empty
+	NatsCon *nats.Conn
+	Lock    *spindle.Lock
+	Log     Storer
+}
+
+func New(cfg Config) *Store {
+	s := &Store{cfg.Id, cfg.NatsCon, cfg.Lock, cfg.Log}
+	if s.id == "" {
+		s.id = uuid.NewString()
 	}
 
-	return nil
+	return s
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.17
 
 require (
 	github.com/flowerinthenight/spindle v0.4.5
+	github.com/google/uuid v1.2.0
 	github.com/nats-io/nats.go v1.12.1
 )
 
@@ -13,7 +14,6 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/google/uuid v1.2.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.1.0 // indirect
 	github.com/nats-io/nats-server/v2 v2.5.0 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
