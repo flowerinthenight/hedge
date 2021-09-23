237071c218561f28f2a91e8c45a9da72f783ca9a
flowerinthenight <flowerinthenight@pm.me>
2021-09-24 05:48:51 +0900

    chore: use pod ip as id

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -13,8 +13,10 @@ import (
 )
 
 var (
-	dbstr        = flag.String("db", "", "fmt: projects/{v}/instances/{v}/databases/{v}")
-	group        = flag.String("group", "dstore-demo-group", "group name, common to all instances")
+	dbstr = flag.String("db", "", "fmt: projects/{v}/instances/{v}/databases/{v}")
+	group = flag.String("group", "dstore-demo-group", "group name, common to all instances")
+	// This is set in the deployment_template.yaml.
+	id           = flag.String("id", os.Getenv("K8S_POD_IP"), "this instance's unique id within the group")
 	spindleTable = flag.String("spindletable", "testlease", "see https://github.com/flowerinthenight/spindle for more info")
 	logTable     = flag.String("logtable", "testdstore_log", "the table for our log data")
 )
@@ -29,7 +31,7 @@ func main() {
 
 	s := dstore.New(dstore.Config{
 		GroupName:     *group,
-		Id:            "dstore-demo-1",
+		Id:            *id,
 		SpannerClient: client,
 		SpindleTable:  *spindleTable,
 		LogTable:      *logTable,
