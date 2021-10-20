22ad8f6f0b1a06d5bfb795c909d896a0717f6680
flowerinthenight <flowerinthenight@pm.me>
2021-10-20 17:48:08 +0900

    test: use ddlfile

--- testdata/emuddl.sql
+++ testdata/emuddl.sql
@@ -1,16 +1,2 @@
--- for spindle
-CREATE TABLE locktable
-    (name STRING(MAX) NOT NULL,
-    heartbeat TIMESTAMP OPTIONS (allow_commit_timestamp=true),
-    token TIMESTAMP OPTIONS (allow_commit_timestamp=true),
-    writer STRING(MAX)
-) PRIMARY KEY (name);
-
--- for hedge
-CREATE TABLE logtable
-    (id STRING(MAX),
-    key STRING(MAX),
-    value STRING(MAX),
-    leader STRING(MAX),
-    timestamp TIMESTAMP OPTIONS (allow_commit_timestamp=true)
-) PRIMARY KEY (key, id);
+CREATE TABLE locktable (name STRING(MAX) NOT NULL, heartbeat TIMESTAMP OPTIONS (allow_commit_timestamp=true), token TIMESTAMP OPTIONS (allow_commit_timestamp=true), writer STRING(MAX)) PRIMARY KEY (name);
+CREATE TABLE logtable (id STRING(MAX), key STRING(MAX), value STRING(MAX), leader STRING(MAX), timestamp TIMESTAMP OPTIONS (allow_commit_timestamp=true)) PRIMARY KEY (key, id);
