2e98452b2126dd019ab608cca0d853cae34e7c5e
flowerinthenight <flowerinthenight@pm.me>
2021-10-20 17:54:22 +0900

    test: use ddlfile

--- testdata/emuddl.sql
+++ testdata/emuddl.sql
@@ -1,1 +1,2 @@
-CREATE TABLE locktable (name STRING(MAX) NOT NULL, heartbeat TIMESTAMP OPTIONS (allow_commit_timestamp=true), token TIMESTAMP OPTIONS (allow_commit_timestamp=true), writer STRING(MAX)) PRIMARY KEY (name); CREATE TABLE logtable (id STRING(MAX), key STRING(MAX), value STRING(MAX), leader STRING(MAX), timestamp TIMESTAMP OPTIONS (allow_commit_timestamp=true)) PRIMARY KEY (key, id);
+CREATE TABLE locktable (name STRING(MAX) NOT NULL, heartbeat TIMESTAMP OPTIONS (allow_commit_timestamp=true), token TIMESTAMP OPTIONS (allow_commit_timestamp=true), writer STRING(MAX)) PRIMARY KEY (name);
+CREATE TABLE logtable (id STRING(MAX), key STRING(MAX), value STRING(MAX), leader STRING(MAX), timestamp TIMESTAMP OPTIONS (allow_commit_timestamp=true)) PRIMARY KEY (key, id)
