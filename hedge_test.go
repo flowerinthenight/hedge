c8cd318d2acc61edb868a39b63eeced780ffe9b1
flowerinthenight <flowerinthenight@pm.me>
2021-10-20 21:12:10 +0900

    test: wip op

--- hedge_test.go
+++ hedge_test.go
@@ -1,7 +1,75 @@
 package hedge
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	gaxv2 "github.com/googleapis/gax-go/v2"
+)
+
+const (
+	db = "projects/test-project/instances/test-instance/databases/testdb"
+)
 
 func TestBasic(t *testing.T) {
-	t.Log("wip")
+	ctx := context.Background()
+	client, err := spanner.NewClient(ctx, db)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	defer client.Close()
+	op := New(client, ":8080", "locktable", "mylock", "logtable",
+		WithLeaderHandler(
+			nil,
+			func(data interface{}, msg []byte) ([]byte, error) {
+				t.Log("[send] received:", string(msg))
+				return []byte("send " + string(msg)), nil
+			},
+		),
+		WithBroadcastHandler(
+			nil,
+			func(data interface{}, msg []byte) ([]byte, error) {
+				t.Log("[broadcast/semaphore] received:", string(msg))
+				return nil, nil
+			},
+		),
+	)
+
+	done := make(chan error, 1)
+	quit, cancel := context.WithCancel(ctx)
+	go op.Run(quit, done)
+
+	var cnt int
+	bo := gaxv2.Backoff{
+		Initial:    time.Second,
+		Max:        time.Second * 30,
+		Multiplier: 2,
+	}
+
+	for {
+		cnt++
+		locked, token := op.HasLock()
+		switch {
+		case locked:
+			t.Logf("lock obtained, token=%v", token)
+			break
+		default:
+			t.Log("lock not obtained, retry")
+			time.Sleep(bo.Pause())
+			continue
+		}
+
+		if cnt >= 10 {
+			t.Fatalf("can't get lock")
+		}
+
+		break
+	}
+
+	cancel()
+	<-done
 }
