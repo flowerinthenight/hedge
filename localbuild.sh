101dea16e1049233eff0e145f4d785c2321dd5ce
flowerinthenight <root@flowerinthenight.com>
2025-09-05 10:22:32 +0900

    ci skip: update localbuild

--- localbuild.sh
+++ localbuild.sh
@@ -3,4 +3,4 @@ docker build --rm -t demo .
 docker tag demo asia.gcr.io/mobingi-main/hedge:$1
 docker push asia.gcr.io/mobingi-main/hedge:$1
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc) -f
-sed -i -e 's/image\:\ asia.gcr.io\/mobingi\-main\/hedge[\:@].*$/image\:\ asia.gcr.io\/mobingi\-main\/hedge\:'$1'/g' deployment.yaml
+[ -f deployment-private.yaml ] && sed -i -e 's/image\:\ asia.gcr.io\/mobingi\-main\/hedge[\:@].*$/image\:\ asia.gcr.io\/mobingi\-main\/hedge\:'$1'/g' deployment-private.yaml
