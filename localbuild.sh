beead0fd2aabf6cb9eb3dd6ccd9fdea865089db7
flowerinthenight <root@flowerinthenight.com>
2025-09-18 00:18:41 +0900

    ci skip: update scripts and dockerfile

--- localbuild.sh
+++ localbuild.sh
@@ -1,6 +1,6 @@
 kubectl delete -f deployment.yaml
-docker build --rm -t demo .
-docker tag demo asia.gcr.io/mobingi-main/hedge:$1
-docker push asia.gcr.io/mobingi-main/hedge:$1
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc) -f
+DOCKER_BUILDKIT=0 docker build --rm -t demo .
+DOCKER_BUILDKIT=0 docker tag demo asia.gcr.io/mobingi-main/hedge:$1
+DOCKER_BUILDKIT=0 docker push asia.gcr.io/mobingi-main/hedge:$1
+DOCKER_BUILDKIT=0 docker rmi $(docker images --filter "dangling=true" -q --no-trunc) -f
 [ -f deployment-private.yaml ] && sed -i -e 's/image\:\ asia.gcr.io\/mobingi\-main\/hedge[\:@].*$/image\:\ asia.gcr.io\/mobingi\-main\/hedge\:'$1'/g' deployment-private.yaml
